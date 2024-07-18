c4ee3196cb9c64c47c01de1bcd4696a6dafe0891
zuzanna-orzechowska <zuzannaorzechowska4@gmail.com>
2024-07-18 15:02:31 +0200

    responsive design for "menu-slider-section" and fixed some design for "about-us"

--- app.js
+++ app.js
@@ -6,3 +6,22 @@ function changeMenuIcon() {
     menu.classList.toggle("ri-close-large-line");
     navListUl.classList.toggle("nav-open");
 };
+
+// resizing image - slider (so img will take whole space of div element)
+document.addEventListener('DOMContentLoaded', function() { //when website is fully loaded
+    const swiperSlides = document.querySelectorAll('.swiper-slide'); //NodeList
+    
+    function resizeImages() {
+        swiperSlides.forEach(slide => { //slide is single element in div ".swiper-slide"
+            const img = slide.querySelector('img');
+            if (img) {
+                img.style.height = slide.clientHeight + 'px'; //height of an element in pixels, including padding, but not the border, scrollbar or margin
+            }
+        });
+    }
+
+    resizeImages();
+
+    //when changing the width of the website
+    window.addEventListener('resize', resizeImages);
+});
